8f43a9c1ab2c4af469ee4c9d577be8899407884b
Joao Duarte <joaogc633@gmail.com>
2024-05-16 16:05:36 -0300

    Remoção do header e adição de ícones na tabbar

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Home } from './pages/home'
 import { Passwords } from './pages/passwords'
+import {Ionicons} from '@expo/vector-icons'
 
 const Tab = createBottomTabNavigator()
 
@@ -10,11 +11,29 @@ export function Routes() {
             <Tab.Screen
                 name="home"
                 component={Home}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({focused, size, color}) => {
+                        if(focused){
+                            return <Ionicons size={size} color={color} name='home'/>
+                        }
+                        return <Ionicons size={size} color={color} name='home-outline'/>
+                    }
+                }}
             />
 
             <Tab.Screen
                 name="passwords"
                 component={Passwords}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({focused, size, color}) => {
+                        if(focused){
+                            return <Ionicons size={size} color={color} name="lock-closed"/>
+                        }
+                        return <Ionicons size={size} color={color} name="lock-closed-outline"/>
+                    }
+                }}
             />
         </Tab.Navigator>
     )
